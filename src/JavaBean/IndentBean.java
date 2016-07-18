777a3064795fbce9dc749feb26f8dcce6a1703eb
Macpotty <645092284@qq.com>
2016-07-19 01:02:57 +0800

    try a new PL

---
+++ src/JavaBean/IndentBean.java
@@ -0,0 +1,55 @@
+package JavaBean;
+
+/**
+ * @Author: michael
+ * @Date: 16-7-19 上午12:49
+ * @Project: S.M.
+ * @Package: JavaBean
+ */
+public class IndentBean {
+    private int id;
+    private int uid;
+    private int mid;
+    private String date;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
